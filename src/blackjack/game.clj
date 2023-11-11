3302f8ca7b048d228099b518cb55d12e2730b9cf
rvjs <renato.valentim@tempest.com.br>
2023-11-11 06:01:00 -0300

    feat: make a player

--- src/blackjack/game.clj
+++ src/blackjack/game.clj
@@ -2,3 +2,7 @@
 
 (defn new-card []
   (inc (rand-int 13)))
+
+(defn make-player [name]
+  (let [card-1 new-card card-2 new-card]
+    {:name name :cards [card-1 card-2]}))
--- test/blackjack/game_test.clj
+++ test/blackjack/game_test.clj
@@ -1,10 +1,16 @@
 (ns blackjack.game-test
-  (:require [clojure.test :refer :all]
-            [blackjack.game :refer [new-card]]))
+  (:require
+   [blackjack.game :refer [make-player new-card]]
+   [clojure.test :refer :all]))
 
 (deftest get-new-card-test
   (testing "Should get a new card more or equals 1"
     (is (<= 1 (new-card))))
 
   (testing "Should get a new card more or equals 13"
-    (is (>= 13 (new-card)))))
+    (is (>= 13 (new-card))))
+
+  (testing "Should define a new player"
+    (let [player (make-player "John Doe")]
+      (is "John Doe" (:name player))
+      (is 2 (count (:cards player))))))
