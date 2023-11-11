516e6619259befb93b660807e5f92907c53fa655
rvjs <renato.valentim@tempest.com.br>
2023-11-11 04:07:13 -0300

    setup: add lein-eftest plugin

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
   :main ^:skip-aot blackjack.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
+  :plugins [[lein-eftest "0.6.0"]])
